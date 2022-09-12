c7843bb4d785a4249953d215a9316a316f930fc8
Spencer C. Imbleau <spencer@imbleau.com>
2022-09-11 21:49:04 -0400

    impl Calculable for Time

--- src/units/time.rs
+++ src/units/time.rs
@@ -1,10 +1,16 @@
+use crate::functions::{Calculable, Calculation};
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum Time {
     Seconds(f64),
     Milliseconds(f64),
     Percent(f64),
     Duration(std::time::Duration),
+    // Special
+    Calc(Box<Calculation<Time>>),
 }
+
+impl Calculable for Time {}
 
 impl std::fmt::Display for Time {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -13,6 +19,35 @@ impl std::fmt::Display for Time {
             Time::Milliseconds(v) => write!(f, "{}ms", v),
             Time::Percent(v) => write!(f, "{}%", v),
             Time::Duration(v) => write!(f, "{}ms", v.as_millis()),
+            Time::Calc(v) => v.fmt(f),
         }
+    }
+}
+
+impl std::ops::Add for Time {
+    type Output = Time;
+    fn add(self, rhs: Self) -> Self::Output {
+        Time::Calc(Box::new(Calculation::Add { lhs: self, rhs }))
+    }
+}
+
+impl std::ops::Sub for Time {
+    type Output = Time;
+    fn sub(self, rhs: Self) -> Self::Output {
+        Time::Calc(Box::new(Calculation::Sub { lhs: self, rhs }))
+    }
+}
+
+impl std::ops::Mul for Time {
+    type Output = Time;
+    fn mul(self, rhs: Self) -> Self::Output {
+        Time::Calc(Box::new(Calculation::Mul { lhs: self, rhs }))
+    }
+}
+
+impl std::ops::Div for Time {
+    type Output = Time;
+    fn div(self, rhs: Self) -> Self::Output {
+        Time::Calc(Box::new(Calculation::Div { lhs: self, rhs }))
     }
 }
