cc591547c5cefaa4150881bce46d93d30f0bb272
Spencer C. Imbleau <spencer@imbleau.com>
2022-09-11 21:35:39 -0400

    Implemented 4 standard ops for lengths

--- src/units/length.rs
+++ src/units/length.rs
@@ -50,3 +50,24 @@ impl std::ops::Add for Length {
         Length::Calc(Box::new(Calculation::Add { lhs: self, rhs }))
     }
 }
+
+impl std::ops::Sub for Length {
+    type Output = Length;
+    fn sub(self, rhs: Self) -> Self::Output {
+        Length::Calc(Box::new(Calculation::Sub { lhs: self, rhs }))
+    }
+}
+
+impl std::ops::Mul for Length {
+    type Output = Length;
+    fn mul(self, rhs: Self) -> Self::Output {
+        Length::Calc(Box::new(Calculation::Mul { lhs: self, rhs }))
+    }
+}
+
+impl std::ops::Div for Length {
+    type Output = Length;
+    fn div(self, rhs: Self) -> Self::Output {
+        Length::Calc(Box::new(Calculation::Div { lhs: self, rhs }))
+    }
+}
