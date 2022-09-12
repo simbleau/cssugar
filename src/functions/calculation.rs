72532c759b2605ead6cd6820bbcaa28edacd41ab
Spencer C. Imbleau <spencer@imbleau.com>
2022-09-11 21:50:11 -0400

    Added rustfmt

--- src/functions/calculation.rs
+++ src/functions/calculation.rs
@@ -1,6 +1,11 @@
 pub(crate) mod markers {
     pub trait Calculable:
-        std::fmt::Display + std::ops::Add + std::ops::Sub + std::ops::Mul + std::ops::Div + Sized
+        std::fmt::Display
+        + std::ops::Add
+        + std::ops::Sub
+        + std::ops::Mul
+        + std::ops::Div
+        + Sized
     {
     }
 }
