522feb4081ad4c40d5e4526f2c5a64622ecf8de5
Spencer C. Imbleau <spencer@imbleau.com>
2022-09-11 21:46:23 -0400

    impl Calculable for Angle

--- src/units/angle.rs
+++ src/units/angle.rs
@@ -1,3 +1,5 @@
+use crate::functions::{Calculable, Calculation};
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum Angle {
     Deg(f64),
@@ -5,7 +7,11 @@ pub enum Angle {
     Rad(f64),
     Turn(f64),
     Percent(f64),
+
+    Calc(Box<Calculation<Angle>>),
 }
+
+impl Calculable for Angle {}
 
 impl std::fmt::Display for Angle {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -15,6 +21,35 @@ impl std::fmt::Display for Angle {
             Angle::Rad(v) => write!(f, "{}rad", v),
             Angle::Turn(v) => write!(f, "{}turn", v),
             Angle::Percent(v) => write!(f, "{}%", v),
+            Angle::Calc(v) => v.fmt(f),
         }
+    }
+}
+
+impl std::ops::Add for Angle {
+    type Output = Angle;
+    fn add(self, rhs: Self) -> Self::Output {
+        Angle::Calc(Box::new(Calculation::Add { lhs: self, rhs }))
+    }
+}
+
+impl std::ops::Sub for Angle {
+    type Output = Angle;
+    fn sub(self, rhs: Self) -> Self::Output {
+        Angle::Calc(Box::new(Calculation::Sub { lhs: self, rhs }))
+    }
+}
+
+impl std::ops::Mul for Angle {
+    type Output = Angle;
+    fn mul(self, rhs: Self) -> Self::Output {
+        Angle::Calc(Box::new(Calculation::Mul { lhs: self, rhs }))
+    }
+}
+
+impl std::ops::Div for Angle {
+    type Output = Angle;
+    fn div(self, rhs: Self) -> Self::Output {
+        Angle::Calc(Box::new(Calculation::Div { lhs: self, rhs }))
     }
 }
