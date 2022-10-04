82b8c9335f5fd3dc7915924c9f708615548962a5
Spencer C. Imbleau <spencer@imbleau.com>
2022-10-03 20:28:03 -0400

    Adjusted prelude imports

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,10 @@ pub mod math;
 
 pub mod prelude {
     pub use crate::colors::*;
+    pub use crate::dimensions::Angle::*;
+    pub use crate::dimensions::Length::*;
+    pub use crate::dimensions::Resolution::*;
+    pub use crate::dimensions::Time::*;
     pub use crate::dimensions::*;
     pub use crate::math::ops::*;
 }
