074dd0feaf0ce0dbba87de2f8f6c5b583ec1425e
Spencer C. Imbleau <spencer@imbleau.com>
2022-12-26 23:15:23 -0500

    Added composability to min/max

--- src/math/function.rs
+++ src/math/function.rs
@@ -1,7 +1,6 @@
-use crate::math::__markers::{Addable, Calculable, Scalable};
+use crate::math::__markers::{Addable, Calculable, Comparable, Scalable};
 use std::marker::PhantomData;
 
-// TODO: Rename to Operator
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub(crate) enum Operation {
     Add,
@@ -31,6 +30,27 @@ impl<Unit, L, R> Function<Unit, L, R> {
     }
 }
 
+impl<Unit, L, R> std::fmt::Display for Function<Unit, L, R>
+where
+    L: std::fmt::Display,
+    R: std::fmt::Display,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self.op {
+                Operation::Add => format!("calc({}+{})", self.lhs, self.rhs),
+                Operation::Sub => format!("calc({}-{})", self.lhs, self.rhs),
+                Operation::Mul => format!("calc({}*{})", self.lhs, self.rhs),
+                Operation::Div => format!("calc({}/{})", self.lhs, self.rhs),
+                Operation::Min => format!("min({},{})", self.lhs, self.rhs),
+                Operation::Max => format!("max({},{})", self.lhs, self.rhs),
+            }
+        )
+    }
+}
+
 impl<Unit, L, R> Calculable<Unit> for Function<Unit, L, R>
 where
     L: Calculable<Unit>,
@@ -52,24 +72,17 @@ where
 {
 }
 
-impl<Unit, L, R> std::fmt::Display for Function<Unit, L, R>
+impl<Unit, L, R> Comparable<Unit> for Function<Unit, L, R>
 where
-    L: std::fmt::Display,
-    R: std::fmt::Display,
+    L: Comparable<Unit>,
+    R: Comparable<Unit>,
 {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self.op {
-                Operation::Add => format!("calc({}+{})", self.lhs, self.rhs),
-                Operation::Sub => format!("calc({}-{})", self.lhs, self.rhs),
-                Operation::Mul => format!("calc({}*{})", self.lhs, self.rhs),
-                Operation::Div => format!("calc({}/{})", self.lhs, self.rhs),
-                Operation::Min => format!("min({},{})", self.lhs, self.rhs),
-                Operation::Max => format!("max({},{})", self.lhs, self.rhs),
-            }
-        )
+    fn min<Rhs>(self, rhs: Rhs) -> Function<Unit, Self, Rhs> {
+        Function::new(self, rhs, Operation::Min)
+    }
+
+    fn max<Rhs>(self, rhs: Rhs) -> Function<Unit, Self, Rhs> {
+        Function::new(self, rhs, Operation::Max)
     }
 }
 
@@ -118,196 +131,56 @@ mod tests {
     use crate::prelude::*;
 
     #[test]
-    fn test_calc() {
-        let c1 = Length::Vw(100.);
-        let c2 = Length::Vw(100.) + Length::Px(300.);
-        assert_eq!(format!("{}", c1 + c2), "calc(100vw + calc(100vw + 300px))");
-        assert_eq!(format!("{}", c1 - c2), "calc(100vw - calc(100vw + 300px))");
-        assert_eq!(format!("{}", c1 * c2), "calc(100vw * calc(100vw + 300px))");
-        assert_eq!(format!("{}", c1 / c2), "calc(100vw / calc(100vw + 300px))");
+    fn test_level1() {
+        let l1 = Length::Vw(100.);
+        let l2 = Length::Vw(100.);
+        assert_eq!(format!("{}", l1 + l2), "calc(100vw+100vw)");
+        assert_eq!(format!("{}", l1 - l2), "calc(100vw-100vw)");
+        assert_eq!(format!("{}", l1 * l2), "calc(100vw*100vw)");
+        assert_eq!(format!("{}", l1 / l2), "calc(100vw/100vw)");
+        assert_eq!(format!("{}", l1.min(l2)), "min(100vw,100vw)");
+        assert_eq!(format!("{}", l1.max(l2)), "max(100vw,100vw)");
     }
 
     #[test]
-    fn test_calc2() {
-        let c1 = Length::Vw(100.) + Length::Px(300.);
-        let c2 = Length::Vw(100.) + Length::Px(300.);
-        assert_eq!(
-            format!("{}", c1 + c2),
-            "calc(calc(100vw + 300px) + calc(100vw + 300px))"
-        );
-        assert_eq!(
-            format!("{}", c1 - c2),
-            "calc(calc(100vw + 300px) - calc(100vw + 300px))"
-        );
-        assert_eq!(
-            format!("{}", c1 * c2),
-            "calc(calc(100vw + 300px) * calc(100vw + 300px))"
-        );
-        assert_eq!(
-            format!("{}", c1 / c2),
-            "calc(calc(100vw + 300px) / calc(100vw + 300px))"
-        );
+    fn test_level2() {
+        let l1 = Length::Vw(100.);
+        let l2 = Length::Vw(100.) + Length::Px(300.);
+        assert_eq!(format!("{}", l1 + l2), "calc(100vw+calc(100vw+300px))");
+        assert_eq!(format!("{}", l1 - l2), "calc(100vw-calc(100vw+300px))");
+        assert_eq!(format!("{}", l1 * l2), "calc(100vw*calc(100vw+300px))");
+        assert_eq!(format!("{}", l1 / l2), "calc(100vw/calc(100vw+300px))");
+        assert_eq!(format!("{}", l1.min(l2)), "min(100vw,calc(100vw+300px))");
+        assert_eq!(format!("{}", l1.max(l2)), "max(100vw,calc(100vw+300px))");
     }
 
     #[test]
-    fn test_composition_integer() {
-        let int = 2;
-
-        let lc1 = Length::Vw(100.) + Length::In(2.5);
-        let lc2 = Length::Percent(25.) * int;
+    fn test_level3() {
+        let l1 = Length::Px(50.0) + Length::Px(50.);
+        let l2 = Length::Vw(100.) + Length::Px(300.);
         assert_eq!(
-            format!("{}", lc1 + lc2),
-            "calc(calc(100vw + 2.5in) + calc(25% * 2))"
+            format!("{}", l1 + l2),
+            "calc(calc(50px+50px)+calc(100vw+300px))"
         );
         assert_eq!(
-            format!("{}", lc1 - lc2),
-            "calc(calc(100vw + 2.5in) - calc(25% * 2))"
+            format!("{}", l1 - l2),
+            "calc(calc(50px+50px)-calc(100vw+300px))"
         );
         assert_eq!(
-            format!("{}", lc1 * lc2),
-            "calc(calc(100vw + 2.5in) * calc(25% * 2))"
+            format!("{}", l1 * l2),
+            "calc(calc(50px+50px)*calc(100vw+300px))"
         );
         assert_eq!(
-            format!("{}", lc1 / lc2),
-            "calc(calc(100vw + 2.5in) / calc(25% * 2))"
-        );
-
-        let ac1 = Angle::Percent(50.) + Angle::Deg(180.);
-        let ac2 = Angle::Percent(25.) * int;
-        assert_eq!(
-            format!("{}", ac1 + ac2),
-            "calc(calc(50% + 180deg) + calc(25% * 2))"
+            format!("{}", l1 / l2),
+            "calc(calc(50px+50px)/calc(100vw+300px))"
         );
         assert_eq!(
-            format!("{}", ac1 - ac2),
-            "calc(calc(50% + 180deg) - calc(25% * 2))"
+            format!("{}", l1.min(l2)),
+            "min(calc(50px+50px),calc(100vw+300px))"
         );
         assert_eq!(
-            format!("{}", ac1 * ac2),
-            "calc(calc(50% + 180deg) * calc(25% * 2))"
-        );
-        assert_eq!(
-            format!("{}", ac1 / ac2),
-            "calc(calc(50% + 180deg) / calc(25% * 2))"
-        );
-
-        let rc1 = Resolution::Dpi(50.) + Resolution::Dppx(100.);
-        let rc2 = Resolution::Dpi(25.) * int;
-        assert_eq!(
-            format!("{}", rc1 + rc2),
-            "calc(calc(50dpi + 100dppx) + calc(25dpi * 2))"
-        );
-        assert_eq!(
-            format!("{}", rc1 - rc2),
-            "calc(calc(50dpi + 100dppx) - calc(25dpi * 2))"
-        );
-        assert_eq!(
-            format!("{}", rc1 * rc2),
-            "calc(calc(50dpi + 100dppx) * calc(25dpi * 2))"
-        );
-        assert_eq!(
-            format!("{}", rc1 / rc2),
-            "calc(calc(50dpi + 100dppx) / calc(25dpi * 2))"
-        );
-
-        let tc1 = Time::Seconds(5.) + Time::Milliseconds(500.);
-        let tc2 = Time::Percent(25.) * int;
-        assert_eq!(
-            format!("{}", tc1 + tc2),
-            "calc(calc(5s + 500ms) + calc(25% * 2))"
-        );
-        assert_eq!(
-            format!("{}", tc1 - tc2),
-            "calc(calc(5s + 500ms) - calc(25% * 2))"
-        );
-        assert_eq!(
-            format!("{}", tc1 * tc2),
-            "calc(calc(5s + 500ms) * calc(25% * 2))"
-        );
-        assert_eq!(
-            format!("{}", tc1 / tc2),
-            "calc(calc(5s + 500ms) / calc(25% * 2))"
-        );
-    }
-
-    #[test]
-    fn test_composition_number() {
-        let num = 2.0;
-
-        let lc1 = Length::Vw(100.) + Length::In(2.5);
-        let lc2 = Length::Percent(25.) * num;
-        assert_eq!(
-            format!("{}", lc1 + lc2),
-            "calc(calc(100vw + 2.5in) + calc(25% * 2))"
-        );
-        assert_eq!(
-            format!("{}", lc1 - lc2),
-            "calc(calc(100vw + 2.5in) - calc(25% * 2))"
-        );
-        assert_eq!(
-            format!("{}", lc1 * lc2),
-            "calc(calc(100vw + 2.5in) * calc(25% * 2))"
-        );
-        assert_eq!(
-            format!("{}", lc1 / lc2),
-            "calc(calc(100vw + 2.5in) / calc(25% * 2))"
-        );
-
-        let ac1 = Angle::Percent(50.) + Angle::Deg(180.);
-        let ac2 = Angle::Percent(25.) * num;
-        assert_eq!(
-            format!("{}", ac1 + ac2),
-            "calc(calc(50% + 180deg) + calc(25% * 2))"
-        );
-        assert_eq!(
-            format!("{}", ac1 - ac2),
-            "calc(calc(50% + 180deg) - calc(25% * 2))"
-        );
-        assert_eq!(
-            format!("{}", ac1 * ac2),
-            "calc(calc(50% + 180deg) * calc(25% * 2))"
-        );
-        assert_eq!(
-            format!("{}", ac1 / ac2),
-            "calc(calc(50% + 180deg) / calc(25% * 2))"
-        );
-
-        let rc1 = Resolution::Dpi(50.) + Resolution::Dppx(100.);
-        let rc2 = Resolution::Dpi(25.) * num;
-        assert_eq!(
-            format!("{}", rc1 + rc2),
-            "calc(calc(50dpi + 100dppx) + calc(25dpi * 2))"
-        );
-        assert_eq!(
-            format!("{}", rc1 - rc2),
-            "calc(calc(50dpi + 100dppx) - calc(25dpi * 2))"
-        );
-        assert_eq!(
-            format!("{}", rc1 * rc2),
-            "calc(calc(50dpi + 100dppx) * calc(25dpi * 2))"
-        );
-        assert_eq!(
-            format!("{}", rc1 / rc2),
-            "calc(calc(50dpi + 100dppx) / calc(25dpi * 2))"
-        );
-
-        let tc1 = Time::Seconds(5.) + Time::Milliseconds(500.);
-        let tc2 = Time::Percent(25.) * num;
-        assert_eq!(
-            format!("{}", tc1 + tc2),
-            "calc(calc(5s + 500ms) + calc(25% * 2))"
-        );
-        assert_eq!(
-            format!("{}", tc1 - tc2),
-            "calc(calc(5s + 500ms) - calc(25% * 2))"
-        );
-        assert_eq!(
-            format!("{}", tc1 * tc2),
-            "calc(calc(5s + 500ms) * calc(25% * 2))"
-        );
-        assert_eq!(
-            format!("{}", tc1 / tc2),
-            "calc(calc(5s + 500ms) / calc(25% * 2))"
+            format!("{}", l1.max(l2)),
+            "max(calc(50px+50px),calc(100vw+300px))"
         );
     }
 }
